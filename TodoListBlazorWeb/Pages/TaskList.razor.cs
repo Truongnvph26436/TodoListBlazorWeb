cf6e09ae56d44179f1eb87292ffdbd0dcb414558
Trường Nguyễn <truongnvph26436@fpt.edu.vn>
2022-08-13 23:05:46 +0700

    Add Task Detail

---
+++ TodoListBlazorWeb/Pages/TaskList.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TodoList.Model;
+using TodoListBlazorWeb.Services;
+
+namespace TodoListBlazorWeb.Pages
+{
+    public partial class TaskList
+    {
+        [Inject] private ITaskApiClient TaskApiClient { set; get; }
+        private List<TaskDto> Task;
+        protected override async Task OnInitializedAsync()
+        {
+            Task = await TaskApiClient.GetTaskList();
+        }
+    }
+}
